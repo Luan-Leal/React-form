ed2d0c636c1274f37f5f2a1cb12ed89446a165da
Luan dos Santos Leal <luan.leal@compasso.com.br>
2022-06-23 14:33:12 -0300

    feat: first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Home">
+      Home page
+    </div>
+  )
+}
+
+export default Home
