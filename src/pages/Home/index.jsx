0bb90301ae7c669c1df9e7cac20c62863b7f1530
Luan Leal <luan.leal@compasso.com.br>
2022-06-24 11:29:42 -0300

    feat: homepage html and css finished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,18 +4,57 @@ import Input from "../../components/Input"
 import Button from "../../components/Button"
 import Checkbox from "../../components/Checkbox"
 
+import "./index.css"
+import Imagem from "../../assets/data.png"
+
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="Home">
-      <Input />
-      <br />
-      <Button />
-      <br />
-      <Checkbox />
-    </div>
+    <main >
+      <section className='container'>
+
+        <div className='HomeImage'>
+          <img src={ Imagem } alt="" />
+          <p>Intern Sign Up</p>
+        </div>
+      
+        <div className="forms">
+          <div className='one-form-inline'>
+            <Input />
+          </div>
+
+          <div className='two-form-inline'>
+            <div className='form1'>
+              <Input />
+            </div>
+
+            <div className='form2'>
+              <Input />
+            </div>
+          </div>
+
+          <div className='two-form-inline'>
+            <div className='form1'>
+              <Input />
+            </div>
+
+            <div className='form2'>
+              <Input />
+            </div>
+          </div>
+
+          <div className='ButtonAndCheckbox'>
+            <Checkbox />
+            <Button />
+          </div>
+
+        </div>
+
+      </section>
+
+    </main>
   )
 }
 
